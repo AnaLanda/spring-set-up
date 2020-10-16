1a98924ac99316930e32ad70d82b759bc5c2112e
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-10-16 19:02:37 +0300

    added logger

--- src/main/java/spring/setup/Main.java
+++ src/main/java/spring/setup/Main.java
@@ -1,21 +1,24 @@
 package spring.setup;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.setup.config.AppConfig;
+import spring.setup.dao.impl.UserDaoImpl;
 import spring.setup.model.User;
 import spring.setup.service.UserService;
 
 public class Main {
     public static void main(String[] args) {
+        final Logger log = Logger.getLogger(UserDaoImpl.class);
+
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         UserService userService = context.getBean(UserService.class);
 
-        System.out.println("Adding three users to the DB.");
         userService.add(new User("Bojack", "Horseman", "bojack@gmail.com"));
         userService.add(new User("Princess", "Carolyn", "agent@gmail.com"));
         userService.add(new User("Todd", "Chavez", "todd@gmail.com"));
-        System.out.println("\nAll users :\n");
+        log.info("All users : ");
         userService.listUsers().forEach(System.out::println);
     }
 }
