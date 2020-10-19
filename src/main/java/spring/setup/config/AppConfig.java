fabb840e4ee1739eb57ce4a66aa713c1555f1d2c
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-10-19 13:38:46 +0300

    rollback

--- src/main/java/spring/setup/config/AppConfig.java
+++ src/main/java/spring/setup/config/AppConfig.java
@@ -6,31 +6,22 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import spring.setup.model.User;
 
 @Configuration
-@PropertySource("classpath:db.properties")
 @ComponentScan(basePackages = {
         "spring.setup.dao",
         "spring.setup.service"
 })
 public class AppConfig {
-    private final Environment env;
-
-    public AppConfig(Environment env) {
-        this.env = env;
-    }
-
     @Bean
     public DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(env.getProperty("db.driver"));
-        dataSource.setUrl(env.getProperty("db.url"));
-        dataSource.setUsername(env.getProperty("db.username"));
-        dataSource.setPassword(env.getProperty("db.password"));
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/spring?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root1234");
         return dataSource;
     }
 
@@ -39,9 +30,9 @@ public class AppConfig {
         LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
         factoryBean.setDataSource(getDataSource());
         Properties properties = new Properties();
-        properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-        properties.put("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
-        properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.format_sql", "true");
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
         factoryBean.setHibernateProperties(properties);
         factoryBean.setAnnotatedClasses(User.class);
         return factoryBean;
