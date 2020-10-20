1914d3dee941a549c4f02b206ebe469c9e8c6fbd
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-10-20 14:17:32 +0300

    renamed WebAppInitializer

--- src/main/java/spring/setup/config/WebAppInitializer.java
+++ src/main/java/spring/setup/config/WebAppInitializer.java
@@ -2,7 +2,7 @@ package spring.setup.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[]{AppConfig.class};
